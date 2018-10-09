dc0394192226f4214b3d4a7a490f7eacb1752097
AmrAlmagic <amr.abdalrahman94@gmail.com>
2018-10-09 22:00:02 +0200

    add environment variables

--- config/database.js
+++ config/database.js
@@ -1,8 +1,6 @@
-module.exports = {
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./prod-database');
+}else{
+    module.exports = require('./dev-database');
 
-    /*mongoDbUrl: 'mongodb://localhost:27017/cms'*/
-
-
-    mongoDbUrl: 'mongodb://cms-user:Qhz45K{[t5T4j9vG@ds127293.mlab.com:27293/cms'
-
-};
+}
